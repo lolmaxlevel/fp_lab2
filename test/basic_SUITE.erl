90fefcb6735741cc6e7ac9009d53de0c2bf436ab
lolmaxlevel <lolmaxlevel@gmail.com>
2024-10-13 20:02:10 +0300

    lint fixes

--- test/basic_SUITE.erl
+++ test/basic_SUITE.erl
@@ -22,13 +22,13 @@ add_element_test(_) ->
 
 % add a lot of elements to test growing
 grow_test(_) ->
-  #set{storage = Array, length = Length} = add_1k_elements(hashmap_set:new(), 0),
+  #set{storage = Array, length = Length} = add_elements(hashmap_set:new(), 0),
   ?assertEqual(Length, 1000),
   ?assert(array:size(Array) >= 1000).
 
 % add 1000 elements and remove 3 of them to test removing
 remove_test(_) ->
-  Set = add_1k_elements(hashmap_set:new(), 0),
+  Set = add_elements(hashmap_set:new(), 0),
   Set1 = hashmap_set:remove_element(1, Set),
   Set2 = hashmap_set:remove_element(2, Set1),
   Set3 = hashmap_set:remove_element(3, Set2),
@@ -38,15 +38,15 @@ remove_test(_) ->
   ?assertEqual(hashmap_set:get_element(3, Set3), not_found),
   ?assertEqual(hashmap_set:get_element(4, Set3), found).
 
-add_1k_elements(Set, Count) when Count < 1000 ->
+add_elements(Set, Count) when Count < 1000 ->
   NewSet = hashmap_set:add_element(Count, Set),
-  add_1k_elements(NewSet, Count + 1);
+  add_elements(NewSet, Count + 1);
 
-add_1k_elements(Set, _) ->
+add_elements(Set, _) ->
   Set.
 
 filter_test(_) ->
-  Set = add_1k_elements(hashmap_set:new(), 0),
+  Set = add_elements(hashmap_set:new(), 0),
   FilteredSet = hashmap_set:filter(fun(X) -> X > 100 end, Set),
   ?assertEqual(FilteredSet#set.length, 899),
   ?assertEqual(hashmap_set:get_element(1, FilteredSet), not_found),
@@ -55,7 +55,7 @@ filter_test(_) ->
   ?assertEqual(hashmap_set:get_element(101, FilteredSet), found).
 
 to_list_test(_) ->
-  Set = add_1k_elements(hashmap_set:new(), 0),
+  Set = add_elements(hashmap_set:new(), 0),
   List = hashmap_set:to_list(Set),
   ?assertEqual(length(List), 1000),
   ?assertEqual(lists:sort(List), lists:seq(0, 999)).
@@ -69,7 +69,7 @@ from_list_test(_) ->
   ?assertEqual(hashmap_set:get_element(321, Set), found).
 
 map_test(_) ->
-  Set = add_1k_elements(hashmap_set:new(), 0),
+  Set = add_elements(hashmap_set:new(), 0),
   NewSet = hashmap_set:map(fun(X) -> X + 1 end, Set),
   NewSetMultiple = hashmap_set:map(fun(X) -> X * 2 end, NewSet),
   ?assertEqual(NewSet#set.length, 1000),
@@ -78,7 +78,7 @@ map_test(_) ->
   ?assertEqual(lists:sort(hashmap_set:to_list(NewSetMultiple)), [X || X <- lists:seq(1, 2000), X rem 2 =:= 0]).
 
 foldl_test(_) ->
-  Set = add_1k_elements(hashmap_set:new(), 0),
+  Set = add_elements(hashmap_set:new(), 0),
   MultSet = hashmap_set:add_element(2, hashmap_set:add_element(1, hashmap_set:new())),
   Sum = hashmap_set:foldl(fun(X, Acc) -> X + Acc end, 0, Set),
   Multiplied = hashmap_set:foldl(fun(X, Acc) -> X * Acc end, 1, Set),
@@ -88,7 +88,7 @@ foldl_test(_) ->
   ?assertEqual(Multiplied2, 2).
 
 foldr_test(_) ->
-  Set = add_1k_elements(hashmap_set:new(), 0),
+  Set = add_elements(hashmap_set:new(), 0),
   MultSet = hashmap_set:add_element(2, hashmap_set:add_element(1, hashmap_set:new())),
   Sum = hashmap_set:foldr(fun(X, Acc) -> X + Acc end, 0, Set),
   Multiplied = hashmap_set:foldr(fun(X, Acc) -> X * Acc end, 1, Set),
