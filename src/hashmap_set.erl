765ba955633c91dbfd192b26f775c519049ed492
lolmaxlevel <lolmaxlevel@gmail.com>
2024-10-13 17:31:43 +0300

    fixed resizing

--- src/hashmap_set.erl
+++ src/hashmap_set.erl
@@ -5,27 +5,27 @@
 -include("hashmap_set.hrl").
 
 -opaque set(Value) ::
-    #set{storage :: array:array(Value), length :: integer()}.
+#set{storage :: array:array(Value), length :: integer()}.
 
 -export_type([set/1]).
 
 new() ->
-    #set{storage = array:new(?INIT_CAPACITY), length = 0}.
+  #set{storage = array:new(?INIT_CAPACITY), length = 0}.
 
 add_element(Value, #set{storage = Array, length = Length}) ->
-    ResizedArray =
-        case Length / array:size(Array) > ?LOAD_FACTOR of
-            true ->
-                grow_array(Array);
-            false ->
-                Array
-        end,
-    case put_element(Value, ResizedArray) of
-        {changed_value, ReturnedArray} ->
-            #set{storage = ReturnedArray, length = Length};
-        {new_value, ReturnedArray} ->
-            #set{storage = ReturnedArray, length = Length + 1}
-    end.
+  ResizedArray =
+    case Length / array:size(Array) > ?LOAD_FACTOR of
+      true ->
+        grow_array(Array);
+      false ->
+        Array
+    end,
+  case put_element(Value, ResizedArray) of
+    {changed_value, ReturnedArray} ->
+      #set{storage = ReturnedArray, length = Length};
+    {new_value, ReturnedArray} ->
+      #set{storage = ReturnedArray, length = Length + 1}
+  end.
 
 put_element(Value, Array) ->
   Position = calc_hash(Value, Array),
@@ -34,7 +34,7 @@ put_element(Value, Array) ->
       {new_value, array:set(Position, Value, Array)};
     OldValue when OldValue == Value ->
       {changed_value, Array};
-    OldValue when OldValue /= Value->
+    OldValue when OldValue /= Value ->
       FreePosition = find_free_position(Position, Array),
       {new_value, array:set(FreePosition, Value, Array)}
   end.
@@ -50,24 +50,26 @@ find_free_position(Position, Array) ->
   end.
 
 grow_array(Array) ->
-    NewSize = array:size(Array) * ?GROW_FACTOR,
-    NewArray = array:new(NewSize),
-    copy_array(Array, NewArray).
+  NewSize = array:size(Array) * ?GROW_FACTOR,
+  NewArray = array:new(NewSize),
+  copy_array(Array, NewArray).
 
 copy_array(Array, NewArray) ->
-    Size = array:size(Array),
-    copy_array(Array, NewArray, 0, Size).
+  Size = array:size(Array),
+  copy_array(Array, NewArray, 0, Size).
 
-copy_array(Array, NewArray, Index, Size) when Index =< Size ->
-    case array:get(Index, Array) of
-        undefined ->
-            copy_array(Array, NewArray, Index + 1, Size);
-        Value ->
-            NewArray = put_element(Value, NewArray),
-            copy_array(Array, NewArray, Index + 1, Size)
-    end;
+copy_array(Array, NewArray, Index, Size) when Index < Size ->
+  case array:get(Index, Array) of
+    undefined ->
+      copy_array(Array, NewArray, Index + 1, Size);
+    Value ->
+      {_, NewArray1} = put_element(Value, NewArray),
+      io:format("NewArray1: ~p~n", [NewArray1]),
+      copy_array(Array, NewArray1, Index + 1, Size)
+  end;
+
 copy_array(_, NewArray, _, _) ->
-    NewArray.
+  NewArray.
 
 calc_hash(Value, Array) ->
-    erlang:phash2(Value, array:size(Array)).
+  erlang:phash2(Value, array:size(Array)).
--- test/basic_SUITE.erl
+++ test/basic_SUITE.erl
@@ -1,14 +1,28 @@
 -module(basic_SUITE).
 
+-include_lib("../src/hashmap_set.hrl").
 -include_lib("common_test/include/ct.hrl").
 -include_lib("stdlib/include/assert.hrl").
 
-%%-export([all/0]).
-%%-export([fib_data_tests/1, dist_power_data_tests/1]).
+-export([all/0]).
+-export([grow_test/1]).
+all() ->
+  [grow_test].
 
-%%all() ->
-%%    [fib_data_tests, dist_power_data_tests].
-%%
+grow_test(_) ->
+  #set{storage = Array, length = Length} = add_1k_elements(hashmap_set:new(), 0),
+  ?assertEqual(Length, 1000),
+  ?assert(array:size(Array) >= 1000).
+
+
+add_1k_elements(Set, Count) when Count < 1000 ->
+  NewSet = hashmap_set:add_element(Count, Set),
+  add_1k_elements(NewSet, Count + 1);
+
+add_1k_elements(Set, _) ->
+  Set.
+
+
 %%%% Common test requires Config argument in test cases
 %%fib_data_tests(_) ->
 %%    fib_regular_case(4613732).
